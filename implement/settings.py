724d8077a6bee7c99123c3872e81ed392dd38c1e
RicardoMLi <33721335+RicardoMLi@users.noreply.github.com>
2018-11-20 22:16:50 +0800

    修改settings

--- implement/settings.py
+++ implement/settings.py
@@ -4,10 +4,10 @@ IS_SEND = True
 #email settings
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = '508477625@qq.com'
+EMAIL_HOST_USER = your_email_host_user
 
-EMAIL_HOST_PASSWORD = 'kxvnieeszmusbiib'
+EMAIL_HOST_PASSWORD = your_email_host_password
 EMAIL_SUBJECT_PREFIX = 'bilibili top 100 视频'
 EMAIL_USE_TLS = True   #与SMTP服务器通信时，是否启用TLS连接(安全连接)
-EMAIL_SENDER = '508477625@qq.com'
-EMAIL_RECEIVER = '449273863@qq.com'
+EMAIL_SENDER = your_email
+EMAIL_RECEIVER = receiver_email
